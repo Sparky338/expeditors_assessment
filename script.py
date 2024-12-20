b4175b3a4c6e88e64b9e1d45517527ba31b71cd2
Matthew Hutter <matthewjhutter@gmail.com>
2024-12-20 15:14:09 -0800

    minor changes for clarity and ease of reading the output document, minor adjustments to comments

--- script.py
+++ script.py
@@ -12,28 +12,28 @@ read_data = '''INSERT INTO users (first_name, last_name, street, city, state, ag
 def iter_data():
     with open(data) as d:
         for row in d:
-            # remove commas and periods, strip newline, strip " at start and end, split values on ""
-            yield row.replace(",", "").replace(".", "").upper().rstrip().strip('"').split('""')
+            # Remove commas and periods, strip newline, strip " at start and end, split values on ""
+            yield row.replace(",", "").replace(".", "").title().rstrip().strip('"').split('""')
 
 # Remove the previous table if it exists.
 cursor.execute("DROP TABLE IF EXISTS users")
 
-# create a table with the user's name, address, and age.
+# Create a table with the user's name, address, and age.
 cursor.execute("CREATE TABLE users (first_name TEXT NOT NULL, last_name TEXT NOT NULL, street TEXT NOT NULL, city TEXT NOT NULL, state TEXT NOT NULL, age NUM NOT NULL)")
 
 # Bulk add to the table from the provided text file.
 with connection:
     connection.executemany(read_data, iter_data())
 
-# removes extra space at the end of a street.
+# Removes extra space at the end of a street.
 cursor.execute("UPDATE users SET street = trim(street)")
 
-# group by Household (Address and count of occupants),
+# Group by Household (Address and count of occupants),
 household = """SELECT street || " " || city || " " || state, COUNT(first_name)
 FROM users
 GROUP BY street, city, state;"""
 
-# list occupants who are 19+ only: First, last, address, age.
+# List occupants who are 19+ only: First, Last, address, age.
 # Sorted by last name, first name
 members = cursor.execute("""SELECT first_name, last_name, street || " " || city || " " || state, age
 FROM users
@@ -42,21 +42,17 @@ ORDER BY last_name, first_name;""").fetchall()
 
 members_dict = defaultdict(list)
 
-# add household members to a list under the key of their address
+# Add household members to a list under the key of their address
 for i in members:
     members_dict[i[2]].append(i)
 
-# return users on a new line and indented
+# Return users on an indented new line
 def household_members(id):
-    return "\n\t".join(str(i)for i in members_dict[id])
+    return "\n\t".join(str(list(i)) for i in members_dict[id])
 
-
-# print(cursor.execute(members).fetchall())
-
+# Create a new document and write in the grouped, sorted, and ordered data.
 with open(output_file, "w") as o:
     for id in cursor.execute(household):
-        # print(id[0], ",", id[1], "Household occupants", "\n\t", household_members(id[0]))
-        o.write(" ".join((id[0], ",", str(id[1]), "Household occupants", "\n\t", household_members(id[0]), "\n")))
+        o.write("".join((id[0], ",", str(id[1]), "Household Occupants", "\n\t", household_members(id[0]).replace("[","").replace("]","") or "'No one 19 or older lives in this Household.'", "\n")))
 
 connection.close()
-    #     o.write("\n".join(sorted(d.read().splitlines())))
